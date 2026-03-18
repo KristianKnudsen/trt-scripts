08886540631ccf6140ad72d5642ceccb91486b87
root <kskdud123@gmail.com>
2026-03-18 15:14:25 +0000

    added w16a16 hpc eval job

---
+++ hpc/jobs/qwen/eval_W16A16_LOGITS.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=GPUQ
+#SBATCH --account=share-ie-idi
+#SBATCH --gres=gpu:a100:1
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
+#SBATCH --job-name="eval_W16A16_LOGITS"
+#SBATCH -c4
+#SBATCH --time=00-02:00:00
+#SBATCH --begin=now
+
+BASE="/cluster/home/krisskn/master-thesis/trt-scripts"
+MODEL_DIR="/cluster/home/krisskn/master-thesis/hf-cache/models/Qwen2.5-3B"
+CONFIG="eval_config_W16A16_LOGITS.json"
+
+srun /usr/bin/apptainer exec --nv --writable-tmpfs \
+  $BASE/hpc/trtllm-tools.sif \
+  python $BASE/eval/custom_lmeval_wrapper.py \
+    --config $BASE/eval/configs/$CONFIG \
+    --base $BASE \
+    --model-dir $MODEL_DIR
